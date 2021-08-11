3eda871d3535e81a384b675ce259d40658efb242
EtienneRQT <etienne.riquet@sfr.fr>
2021-08-11 23:03:54 +0200

    rendered fake data in templates dynamically

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 import datetime
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
         "author": "Maximilian",
-        "date": date(2021, 7, 21),
+        "date": datetime.date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -27,7 +27,7 @@ posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Maximilian",
-        "date": date(2022, 3, 10),
+        "date": datetime.date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -48,7 +48,7 @@ posts = [
         "slug": "into-the-woods",
         "image": "woods.jpg",
         "author": "Maximilian",
-        "date": date(2020, 8, 5),
+        "date": datetime.date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -69,12 +69,21 @@ posts = [
 
 
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=lambda post: post['date'])
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, 'blog/all-posts.html', {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post,
+    })
